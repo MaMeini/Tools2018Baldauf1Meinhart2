233f1802ffeafab4b30c964c9dae912a46add29d
MaMeini <squls.austrian@gmail.com>
2019-05-21 16:37:20 +0200

    commit von mir 2

--- GitHub-Fred_MaMeini_android/Natursteinmauer_Ü1A2.java
+++ GitHub-Fred_MaMeini_android/Natursteinmauer_Ü1A2.java
@@ -11,6 +11,7 @@ public class Natursteinmauer_Ü1A2 {
 		int Acm = l * h1 - ((h1-h2) * l / 200);
 		int Am = Acm / 10000;
 		Out.println("---- Kosten für das Segment ----");
+		Out.println("wowow”);
 		Out.println("Länge: " + l + " cm, Mittlere Höhe: " + (h1+h2)/2 + " cm");
 		Out.println("Fläche: " + Am + "."+((Acm+50)%10000)/100 +" m2");
 		Out.println("Preis für das Segment: " + Acm * 96/10000 + "."+Acm*96%10000/1000 + Acm*96%1000/100+" EUR");
